56aa21c5f4040cd9dd5bd3469a216d37047c2eb3
Abdullah3772 <abdullah0754520pp@gmail.com>
2026-06-25 14:09:50 -0400

    HICT/2022/39

--- backend/config.php
+++ backend/config.php
@@ -7,5 +7,4 @@ define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'ishis');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-
 define('API_BASE_URL', '/HICT-22013-Hospital-Management-System/backend/api.php');
